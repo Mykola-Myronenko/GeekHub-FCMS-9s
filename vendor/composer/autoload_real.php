0e5a1abfa6564888ca4146e35e334b984c29c2bd
Mykola Myronenko <nicholas.mironenko@gmail.com>
2020-10-17 10:41:59 +0300

    Initial commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitc4ef3855b2e123de18b9554067810291
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
